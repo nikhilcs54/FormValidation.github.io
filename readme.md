96ef942e3bd2b1b0ff9610bce3877ce6a0f9566c
nikhilcs54 <nikhilkumar18101998@gmail.com>
2022-05-22 13:32:45 +0530

    Form-Valid

---
+++ readme.md
@@ -0,0 +1,4 @@
+Step 1:-Copy Bootstrap Starter Template From getbootstrap.com
+Step 2:-Copy "Form Components" from getbootstrap.com
+Step 3:- Grab name,email,phone number using getElementById.
+Step 4:-Apply addEventListener to each grabbed element and and 'blur' event
---
+++ script.js
@@ -0,0 +1,82 @@
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const phone = document.getElementById('phone');
+let validEmail = false;
+let validPhone = false;
+let validUser = false;
+$('#failure').hide();
+$('#success').hide();
+
+name.addEventListener('blur', ()=>{
+    // Validate name here
+    let regex = /^[a-zA-Z]([0-9a-zA-Z]){2,10}$/;
+    let str = name.value;
+    if(regex.test(str)){
+        name.classList.remove('is-invalid');
+        validUser = true;
+    }
+    else{
+        name.classList.add('is-invalid');
+        validUser = false;
+        
+    }
+})
+
+email.addEventListener('blur', ()=>{
+    // Validate email here
+    let regex = /^([_\-\.0-9a-zA-Z]+)@([_\-\.0-9a-zA-Z]+)\.([a-zA-Z]){2,7}$/;
+    let str = email.value;
+    if(regex.test(str)){
+        email.classList.remove('is-invalid');
+        validEmail = true;
+    }
+    else{
+        email.classList.add('is-invalid');
+        validEmail = false;
+    }
+})
+
+phone.addEventListener('blur', ()=>{
+    // Validate phone here
+    let regex = /^([0-9]){10}$/;
+    let str = phone.value;
+    console.log(regex, str);
+    if(regex.test(str)){
+        phone.classList.remove('is-invalid');
+        validPhone = true;
+    }
+    else{
+        phone.classList.add('is-invalid');
+        validPhone = false;
+        
+    }
+})
+
+let submit = document.getElementById('submit');
+submit.addEventListener('click', (e)=>{
+    e.preventDefault();
+
+    
+    
+    // Submit your form here
+    if(validEmail && validUser && validPhone){
+        let failure = document.getElementById('failure');
+
+        
+        let success = document.getElementById('success');
+        success.classList.add('show');
+        $('#failure').hide();
+        $('#success').show();
+        
+    }
+    else{
+        
+        let failure = document.getElementById('failure');
+        failure.classList.add('show');
+        $('#success').hide();
+        $('#failure').show();
+        }
+
+    
+    
+})
